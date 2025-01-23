ca1db400d82824894a98e098f0217bdbee908266
Etienne Gramlich <etgramlich@gmx.de>
2025-01-23 09:55:31 +0100

    Comments on main site (only if comments are present); Init comments inside script; Add error handling through popup-tooltips. Fade-in and fade-out after i.e. 5s. JS variables: use let and const more often.

--- assets/comment.js
+++ assets/comment.js
@@ -1,16 +1,21 @@
 function getRecipeId() {
-  let recipePath = window.location.pathname.substring(0, window.location.pathname.length - 1);
+  const recipePath = window.location.pathname.substring(0, window.location.pathname.length - 1);
   return recipeId = recipePath.substring(recipePath.lastIndexOf('/') + 1);
 };
 
 function queryNumberOfComments() {
-  var commentnummberrequest = new XMLHttpRequest();
+  const commentnummberrequest = new XMLHttpRequest();
   commentnummberrequest.open("GET", "{{ .Site.Params.Backend_url }}/comments/" + getRecipeId(), true);
   commentnummberrequest.onload = (e) => {
     if (commentnummberrequest.readyState === 4) {
       if (commentnummberrequest.status === 200) {
-        document.getElementById('commentsNumber').innerHTML = JSON.parse(commentnummberrequest.responseText);
-        document.getElementById('comments-div').style.removeProperty('display');
+        const numOfComments = JSON.parse(commentnummberrequest.responseText);
+        document.getElementById('commentsNumber').innerHTML = numOfComments;
+        if (numOfComments > 0) {
+          document.getElementById('comments-head').style.removeProperty('display');
+          getComments();
+        }
+        document.getElementById('comments-form').style.removeProperty('display');
       } else {
         console.error(commentnummberrequest.statusText);
       }
@@ -20,23 +25,23 @@ function queryNumberOfComments() {
 };
 
 function queryNumberOfRatings() {
-  let ratingRequest = new XMLHttpRequest();
+  const ratingRequest = new XMLHttpRequest();
   ratingRequest.open("GET", "{{ .Site.Params.Backend_url }}/ratings/" + getRecipeId(), true);
   ratingRequest.onload = (e) => {
     if (ratingRequest.readyState === 4) {
       if (ratingRequest.status === 200) {
-        let rating = parseInt(JSON.parse(ratingRequest.responseText));
+        const rating = parseInt(JSON.parse(ratingRequest.responseText));
         document.getElementById('number-of-ratings').innerHTML = rating;
-        let starContainer = document.getElementById('rating-star-container');
-        let averageRatingRequest = new XMLHttpRequest();
+        const starContainer = document.getElementById('rating-star-container');
+        const averageRatingRequest = new XMLHttpRequest();
         averageRatingRequest.open("GET", "{{ .Site.Params.Backend_url }}/rating/" + getRecipeId(), true);
         averageRatingRequest.onload = (e) => {
           if (averageRatingRequest.readyState === 4) {
             if (averageRatingRequest.status === 200) {
-              let averageRating = parseInt(JSON.parse(averageRatingRequest.responseText));
-              let children = starContainer.children;
-              for (var i = 0; i < children.length && i < averageRating; ++i) {
-                var classes = children[i].classList;
+              const averageRating = parseInt(JSON.parse(averageRatingRequest.responseText));
+              const children = starContainer.children;
+              for (let i = 0; i < children.length && i < averageRating; ++i) {
+                const classes = children[i].classList;
                 if (i < averageRating) {
                   classes.add('bi-star-fill');
                   classes.remove('bi-star');
@@ -61,43 +66,63 @@ function queryNumberOfRatings() {
   ratingRequest.send(null);
 };
 
-document.getElementById('rating-star-container').addEventListener('click', (e) => {
-  let rating = parseInt(e.target.id.substring(e.target.id.length - 1));
-  if (Number.isInteger(rating)) {
-    let postRatingRequest = new XMLHttpRequest();
-    postRatingRequest.open("POST", "{{ .Site.Params.Backend_url }}/rating/" + getRecipeId() + "/" + rating, true);
-    postRatingRequest.onload = (e) => {console.log("Rating sent: " + rating);}
-    postRatingRequest.send();
-    setTimeout(queryNumberOfRatings, 125);
-  }
-});
+function registerStarContainer() {
+  document.getElementById('rating-star-container').addEventListener('click', (e) => {
+    const rating = parseInt(e.target.id.substring(e.target.id.length - 1));
+    if (Number.isInteger(rating)) {
+      const postRatingRequest = new XMLHttpRequest();
+      postRatingRequest.open("POST", "{{ .Site.Params.Backend_url }}/rating/" + getRecipeId() + "/" + rating, true);
+      postRatingRequest.onload = (e) => {console.log("Rating sent: " + rating);}
+      postRatingRequest.send();
+      setTimeout(queryNumberOfRatings, 125);
+    }
+  });
+};
 
 function putComment() {
-  var postRatingRequest = new XMLHttpRequest();
-  postRatingRequest.open("POST", "{{ .Site.Params.Backend_url }}/comment/" + getRecipeId() + "/" + document.getElementById('username').value, true);
+  const username = document.getElementById('username').value;
+  if (!username) {
+    document.getElementById("popup-empty-name").classList.toggle("show");
+    window.setTimeout(() => {document.getElementById("popup-empty-name").classList.toggle("show");}, 5000);
+    return;
+  }
+  const commenttext = document.getElementById('commentText').value;
+  if (!commenttext) {
+    document.getElementById("popup-empty-text").classList.toggle("show");
+    window.setTimeout(() => {document.getElementById("popup-empty-text").classList.toggle("show");}, 5000);
+    return;
+  }
+  const postRatingRequest = new XMLHttpRequest();
+  postRatingRequest.open("POST", "{{ .Site.Params.Backend_url }}/comment/" + getRecipeId() + "/" + username, true);
   postRatingRequest.onload = (e) => {
     if (postRatingRequest.readyState === 4) {
       if (postRatingRequest.status == 200) {
         document.getElementById("comment-form").reset();
         queryNumberOfComments();
+      } else if (postRatingRequest.status == 400) {
+        document.getElementById("popup-invalid-char").classList.toggle("show");
+        window.setTimeout(() => {document.getElementById("popup-invalid-char").classList.toggle("show");}, 5000);
+      } else if (postRatingRequest.status == 403) {
+        document.getElementById("popup-already-commented").classList.toggle("show");
+        window.setTimeout(() => {document.getElementById("popup-already-commented").classList.toggle("show");}, 5000);
       } else {
         console.log(postRatingRequest.statusText);
       }
     }
   }
-  postRatingRequest.send(document.getElementById('commentText').value);
+  postRatingRequest.send(commenttext);
 };
 function getComments() {
-  var getCommentsRequest = new XMLHttpRequest();
+  const getCommentsRequest = new XMLHttpRequest();
   getCommentsRequest.open("GET", "{{ .Site.Params.Backend_url }}/comment/" + getRecipeId(), true);
   getCommentsRequest.onload = (e) => {
     if (getCommentsRequest.readyState === 4) {
       if (getCommentsRequest.status === 200) {
-        let commentsArray = JSON.parse(getCommentsRequest.responseText);
-        let container = document.getElementById("commentsContainer");
+        const commentsArray = JSON.parse(getCommentsRequest.responseText);
+        const container = document.getElementById("commentsContainer");
         container.innerHTML = ""
         for (comment in commentsArray) {
-          let current = commentsArray[comment];
+          const current = commentsArray[comment];
           container.insertAdjacentHTML("beforeend", "<div class='single-comment'><strong>" + current.userName + "</strong> (" + new Date(current.localDateTime).toLocaleString() + ")<br>" + current.commentText + "</div>");
         }
       }
@@ -106,5 +131,39 @@ function getComments() {
   getCommentsRequest.send(null);
 };
 
-queryNumberOfComments();
-queryNumberOfRatings();
+function getCommentsMain(references, titles) {
+  const getCommentsRequest = new XMLHttpRequest();
+  getCommentsRequest.open("GET", "{{ .Site.Params.Backend_url }}/comments/last", true);
+  getCommentsRequest.onload = (e) => {
+    if (getCommentsRequest.readyState === 4) {
+      if (getCommentsRequest.status === 200) {
+        const commentsArray = JSON.parse(getCommentsRequest.responseText);
+        const container = document.getElementById("commentsContainer");
+        container.innerHTML = ""
+        for (comment in commentsArray) {
+          const current = commentsArray[comment];
+          console.log("/recipe/" + current.recipeId + "/");
+          const idx = references.indexOf("/recipe/" + current.recipeId + "/");
+          console.log(idx);
+          container.insertAdjacentHTML("beforeend", "<div class='single-comment'><strong>" + titles[idx] + "</strong><br><strong>" + current.userName + "</strong> (" + new Date(current.localDateTime).toLocaleString() + ")<br>" + current.commentText + "</div>");
+        }
+        if (commentsArray.length > 0) {
+          document.getElementById('comments-div').style.removeProperty('display');
+        }
+      }
+    }
+  }
+  getCommentsRequest.send(null);
+};
+
+// Init recipe site
+if (document.getElementById("comment-form")) {
+  registerStarContainer();
+  queryNumberOfComments();
+  queryNumberOfRatings();
+}
+
+// Init Main site
+if (document.getElementById('welcome')) {
+  getCommentsMain(references, titles);
+}
