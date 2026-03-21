afb1f3bbff604270af184e004d15995509da7c67
Etienne Gramlich <etgramlich@gmx.de>
2026-03-21 21:38:53 +0100

    Import only used part of Bootstrap

---
+++ assets/js/bootstrap.js
@@ -0,0 +1,6 @@
+// Tree-shakable Bootstrap entrypoint: Only include components that are actually used by the site.
+
+// Core component for collapse (navbar / collapse elements)
+import Collapse from './bootstrap/collapse.js'
+
+export { Collapse }
